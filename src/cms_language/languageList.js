5034e2329e810093db6dcd5a8e29282df59257f0
tangbearrrr <rh.sarawasee@gmail.com>
2020-12-25 17:30:38 +0700

    poc drag list

--- src/cms_language/languageList.js
+++ src/cms_language/languageList.js
@@ -1,6 +1,40 @@
 import React from 'react';
+import { SortableContainer, SortableElement, SortableHandle } from "react-sortable-hoc";
 import { List, TextField, EditButton } from 'react-admin';
 import { ListItem, ListItemText } from '@material-ui/core';
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import DragHandleIcon from "@material-ui/icons/DragHandle";
+import arrayMove from "array-move";
+
+
+const DragHandle = SortableHandle(() => (
+    <ListItemIcon>
+        <DragHandleIcon />
+    </ListItemIcon>
+));
+
+const SortableItem = SortableElement(({ ...props }) => (
+    <div style={{ margin: '1em' }}>
+        {props.ids.map(id =>
+            <ListItem>
+                <ListItemText
+                    primary={<TextField record={props.data[id]} source="name" />}
+                />
+                <DragHandle />
+            </ListItem>
+        )}
+    </div>
+));
+
+const SortableListContainer = SortableContainer(({ ...props }) => (
+    <List {...props}>
+        <SortableItem />
+    </List>
+));
+
+// const onSortEnd = ({ oldIndex, newIndex }) => {
+    
+// };
 
 class TestGrid extends React.Component {
     constructor(props) {
@@ -15,7 +49,7 @@ class TestGrid extends React.Component {
                         <ListItemText
                             primary={<TextField record={this.props.data[id]} source="name" />}
                         />
-                        <EditButton resource="posts" basePath={this.props.basePath} record={this.props.data[id]} />
+                        <DragHandle />
                     </ListItem>
                 )}
             </div>
@@ -30,9 +64,12 @@ class LanguageList extends React.Component {
 
     render() {
         return (
-            <List {...this.props}>
-                <TestGrid />
-            </List>
+            <SortableListContainer 
+                {...this.props} 
+                // onSortEnd={onSortEnd}
+                useDragHandle={true}
+                lockAxis="y"
+            />
         );
     }
 }
