4ba0c3ed6b910d196ddc3574f5d65a433f7271ed
Rahat Sarawasee <rahat.sar@ascendcorp.com>
2020-12-27 19:22:07 +0700

    fix list can drag

--- src/cms_language/languageList.js
+++ src/cms_language/languageList.js
@@ -1,67 +1,114 @@
-import React, { useState } from 'react';
-import { SortableContainer, SortableElement, SortableHandle } from "react-sortable-hoc";
-import { List, TextField } from 'react-admin';
-import { ListItem, ListItemText } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle
+} from "react-sortable-hoc";
+import { List, TextField } from "react-admin";
+import { ListItem, ListItemText } from "@material-ui/core";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import DragHandleIcon from "@material-ui/icons/DragHandle";
 import arrayMove from "array-move";
 
-
 const DragHandle = SortableHandle(() => (
-    <ListItemIcon>
-        <DragHandleIcon />
-    </ListItemIcon>
+  <ListItemIcon>
+    <DragHandleIcon />
+  </ListItemIcon>
 ));
 
 const SortableItem = SortableElement(({ ...props }) => {
-    return (
-        <div style={{ margin: '1em' }}>
-            {props.ids.map(id =>
-                <ListItem>
-                    <ListItemText
-                        primary={<TextField record={props.data[id]} source="name" />}
-                    />
-                    <DragHandle />
-                </ListItem>
-            )}
-        </div>
-    )
-})
+  const { data, setItems } = props;
+  console.log("SortableItem");
 
+  useEffect(() => {
+    const list = [];
+    Object.keys(data).map((key) => {
+      list.push({ id: data[key].id, text: data[key].name });
+    });
+    setItems(list);
+  }, [data]);
 
-const SortableListContainer = SortableContainer(({ ...props }) => (
-    <List {...props}>
-        <SortableItem />
-    </List>
-));
+  return (
+    <div style={{ margin: "1em" }}>
+      {props.ids.map((id) => (
+        <ListItem>
+          <ListItemText
+            primary={<TextField record={props.data[id]} source="name" />}
+          />
+          <DragHandle />
+        </ListItem>
+      ))}
+    </div>
+  );
+});
+
+const SortableItem2 = SortableElement(({ text }) => {
+  console.log("SortableItem2");
+
+  return (
+    <div style={{ margin: "1em" }}>
+      <ListItem>
+        <ListItemText primary={text} />
+        <DragHandle />
+      </ListItem>
+    </div>
+  );
+});
+
+const SortableListContainer = SortableContainer(({ ...props }) => {
+  const { items } = props;
+  console.log("items", items);
+  console.log("props", props);
+
+  return (
+    <>
+      {items.length ? (
+        <List {...props}>
+          <>
+            {items.map(({ id, text }, index) => (
+              <SortableItem2 key={id} index={index} text={text} />
+            ))}
+          </>
+        </List>
+      ) : (
+        <List {...props}>
+          <SortableItem {...props} />
+        </List>
+      )}
+    </>
+  );
+});
 
 const SortableList = ({ ...props }) => {
-    const [items, setItems] = useState([props.data]);
+  const [items, setItems] = useState([]);
 
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-        setItems(items => arrayMove(items, oldIndex, newIndex));
-    };
-    
-    return (
-        <SortableListContainer
-            {...props}
-            onSortEnd={onSortEnd}
-            useDragHandle={true}
-            lockAxis="y"
-        />
-    );
-}
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    console.log("oldIndex", oldIndex);
+    console.log("newIndex", newIndex);
+    console.log("onSortEnd items", items);
+    setItems((items) => arrayMove(items, oldIndex, newIndex));
+  };
+
+  return (
+    <SortableListContainer
+      {...props}
+      items={items}
+      setItems={setItems}
+      onSortEnd={onSortEnd}
+      useDragHandle={true}
+      lockAxis="y"
+    />
+  );
+};
 
 class LanguageList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <SortableList {...this.props} />
-        );
-    }
+  render() {
+    return <SortableList {...this.props} />;
+  }
 }
 
 export default LanguageList;
